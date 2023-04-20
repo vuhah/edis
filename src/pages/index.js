b559255a56704cd5534f1adcaef18c3b60cffd61
vuhah <vuuu0412@gmail.com>
2023-04-20 13:51:50 +0700

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,10 +14,9 @@ export default function Home() {
           <code className="font-mono font-bold">src/pages/index.js</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
+          <Link
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/posts/firstpost"
             rel="noopener noreferrer"
           >
             By{' '}
@@ -27,7 +28,7 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
+          </Link>
         </div>
       </div>
 
