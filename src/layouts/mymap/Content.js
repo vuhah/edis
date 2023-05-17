8d0eeca48a02dfeb23846839060764b03955e01e
vuhah <vuuu0412@gmail.com>
2023-05-17 14:16:16 +0700

    logout update

--- src/layouts/mymap/Content.js
+++ src/layouts/mymap/Content.js
@@ -1,12 +1,25 @@
 import { DisasterMap, Dashboard, Friends, Settings } from './subpage'
 import { Layout } from 'antd'
+import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
+import { deleteCookie } from 'cookies-next'
 
 const { Content } = Layout
 
 export default function ContentMap() {
-	const currentSubPage = useSelector((state) => state.mapControl.subPage)
+	const router = useRouter()
 
+	const currentSubPage = useSelector((state) => state.mapControl.subPage)
+	if (currentSubPage === 'logout') {
+		deleteCookie('accessToken')
+		router.push('/')
+	}
+	if (currentSubPage === 'telegram') {
+		router.push('https://t.me/+PxAtq9lsxF9lZmI1', undefined, {
+			target: '_blank',
+			rel: 'noopener noreferrer',
+		})
+	}
 	return (
 		<Content>
 			{currentSubPage === 'map' && <DisasterMap />}
